c9a902cb73060a667fb1638ca740378d982a8e69
hwangs <shwang67@my.bcit.ca>
2025-01-14 21:14:49 -0800

    add 5

--- Pages/ProcessPage/ProcessDetails.cshtml.cs
+++ Pages/ProcessPage/ProcessDetails.cshtml.cs
@@ -2,14 +2,14 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace WebStar.Pages.Shared
+namespace WebStar.Pages
 {
     public class ProcessDetailsModel : PageModel
     {
         public void OnGet(int id)
         {
             var proc = Process.GetProcessById(id);
-            ViewData = 
+            ViewData["Process"] = proc;
         }
     }
 }
